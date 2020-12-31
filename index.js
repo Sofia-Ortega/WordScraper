905516e278529a68c1459501b784e91d5712ecc0
Sofia-Ortega <s.ortega@tamu.edu>
2020-12-30 23:52:10 -0600

    can now download txt file w btn

---
+++ index.js
@@ -0,0 +1,22 @@
+var fs = require('fs');
+var filename = "words/frenchWords.txt"
+
+
+
+
+function getFrenchWords() {
+
+    // fs.readFile(filename, 'utf8', function(err, data) {
+    //     if (err) throw err;
+    //     console.log('OK: ' + filename);
+    //     document.getElementById('words').innerHTML = 'inside fs';
+    //
+    // });
+
+    console.log('working ish')
+
+}
+
+
+
+
